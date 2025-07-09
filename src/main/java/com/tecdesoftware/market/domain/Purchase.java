c75f57da473e685e66d0fd2d9ea230eeacfab85b
marianalarag <marianalarag@outlook.com>
2025-07-09 08:19:10 -0600

    Comportamiento purchase repository

---
+++ src/main/java/com/tecdesoftware/market/domain/Purchase.java
@@ -0,0 +1,70 @@
+package com.tecdesoftware.market.domain;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Purchase {
+    private int pruchaseId;
+    private String clientId;
+    private LocalDateTime date;
+    private String paymentMethod;
+    private String comment;
+    private String state;
+    private List<PurchaseItem> item;
+
+    public int getPruchaseId() {
+        return pruchaseId;
+    }
+
+    public void setPruchaseId(int pruchaseId) {
+        this.pruchaseId = pruchaseId;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public List<PurchaseItem> getItem() {
+        return item;
+    }
+
+    public void setItem(List<PurchaseItem> item) {
+        this.item = item;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+}
